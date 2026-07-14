681d2fc1eb4f075fd91609ff283687c8a6935417
Tok <github@zetoken.net>
2026-07-14 18:28:17 +0200

    Buffer overflow prevention
    - fix: Wrong initialization of a buffer in InstallForLoadCommand
    - update: Removal of some buffers and code simplification

--- WSCT.GlobalPlatform/Commands/InstallForLoadCommand.cs
+++ WSCT.GlobalPlatform/Commands/InstallForLoadCommand.cs
@@ -19,32 +19,17 @@ public class InstallForLoadCommand : CommandAPDU
     public InstallForLoadCommand(Span<byte> loadFileAid, Span<byte> securityDomainAid, Span<byte> loadFileDataBlockHash, Span<byte> loadParameters, Span<byte> loadToken)
         : base(0x80, 0xE6, 0x02, 0x00, 0x00)
     {
-        Udc = new byte[1 + loadFileAid.Length + 1 + securityDomainAid.Length + 3];
-
-        var cursor = Udc.AsSpan();
-
-        cursor[0] = (byte)loadFileAid.Length;
-        cursor = cursor[1..];
-        loadFileAid.CopyTo(cursor);
-        cursor = cursor[loadFileAid.Length..];
-
-        cursor[0] = (byte)securityDomainAid.Length;
-        cursor = cursor[1..];
-        securityDomainAid.CopyTo(cursor);
-        cursor = cursor[securityDomainAid.Length..];
-
-        cursor[0] = (byte)loadFileDataBlockHash.Length;
-        cursor = cursor[1..];
-        loadFileDataBlockHash.CopyTo(cursor);
-        cursor = cursor[loadFileDataBlockHash.Length..];
-
-        cursor[0] = (byte)loadParameters.Length;
-        cursor = cursor[1..];
-        loadParameters.CopyTo(cursor);
-        cursor = cursor[loadParameters.Length..];
-
-        cursor[0] = (byte)loadToken.Length;
-        cursor = cursor[1..];
-        loadToken.CopyTo(cursor);
+        Udc = [
+            (byte)loadFileAid.Length,
+            ..loadFileAid,
+            (byte)securityDomainAid.Length,
+            ..securityDomainAid,
+            (byte)loadFileDataBlockHash.Length,
+            ..loadFileDataBlockHash,
+            (byte)loadParameters.Length,
+            ..loadParameters,
+            (byte)loadToken.Length,
+            ..loadToken
+        ];
     }
 }
--- WSCT.GlobalPlatform/Commands/InstallForMakeSelectableCommand.cs
+++ WSCT.GlobalPlatform/Commands/InstallForMakeSelectableCommand.cs
@@ -19,31 +19,16 @@ public class InstallForMakeSelectableCommand : CommandAPDU
     public InstallForMakeSelectableCommand(Span<byte> applicationAid, Span<byte> privileges, Span<byte> installParameters, Span<byte> installToken)
         : base(0x80, 0xE6, 0x08, 0x00, 0x00)
     {
-        Udc = new byte[1 + 1 + 1 + applicationAid.Length + 1 + privileges.Length + 1 + installParameters.Length + 1 + installToken.Length];
-
-        var cursor = Udc.AsSpan();
-
-        cursor[0] = 0;
-        cursor[1] = 0;
-        cursor = cursor[2..];
-
-        cursor[0] = (byte)applicationAid.Length;
-        cursor = cursor[1..];
-        applicationAid.CopyTo(cursor);
-        cursor = cursor[applicationAid.Length..];
-
-        cursor[0] = (byte)privileges.Length;
-        cursor = cursor[1..];
-        privileges.CopyTo(cursor);
-        cursor = cursor[privileges.Length..];
-
-        cursor[0] = (byte)installParameters.Length;
-        cursor = cursor[1..];
-        installParameters.CopyTo(cursor);
-        cursor = cursor[installParameters.Length..];
-
-        cursor[0] = (byte)installToken.Length;
-        cursor = cursor[1..];
-        installToken.CopyTo(cursor);
+        Udc = [
+            0x00, 0x00,
+            (byte)applicationAid.Length,
+            ..applicationAid,
+            (byte)privileges.Length,
+            ..privileges,
+            (byte)installParameters.Length,
+            ..installParameters,
+            (byte)installToken.Length,
+            ..installToken
+        ];
     }
 }
